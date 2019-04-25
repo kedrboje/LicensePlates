51590d194f1919e5db3ea0c74b1b07863a793079
kedrboje <fmrubchenkov@edu.hse.ru>
2019-04-25 20:46:53 +0300

    initial

---
+++ Dataset_Gen.py
@@ -0,0 +1,41 @@
+import keras
+import json
+import os
+import numpy as np
+import Alphabet_Gen as AG
+
+
+class Dataset_gen():
+
+    def __init__(self, tag: str, dirpath: str = 'dataset/'):
+
+        self.num_sampes = 0
+        self.alp = AG.Alphabet_Gen(tag)
+        self.alp.get_alphabet()
+
+        if tag == 'train':
+            self.dirpath = os.path.join(dirpath, tag)
+            self.ann_train_dirpath = os.path.join(dirpath, tag, 'ann')
+            self.img_train_dirpath = os.path.join(dirpath, tag, 'img')
+
+            for root, dirs, files in os.walk(self.ann_train_dirpath):
+                for file in files:
+                    ann = json.load(open(os.path.join(root, file), 'r'))
+                    desc = ann['description']
+                    if self.alp.is_valid(desc):
+                        print(True) # too much samples
+
+        elif tag == 'test':
+            self.dirpath = os.path.join(dirpath, tag)
+            self.ann_test_dirpath = os.path.join(dirpath, tag, 'ann')
+            self.img_test_dirpath = os.path.join(dirpath, tag, 'img')
+
+            for root, dirs, files in os.walk(self.ann_test_dirpath):
+                for file in files:
+                    ann = json.load(open(os.path.join(root, file), 'r'))
+                    desc = ann['description']
+                    if self.alp.is_valid(desc):
+                        print(True)
+
+
+asd = Dataset_gen(tag='test') # don't use train (see comment above)
