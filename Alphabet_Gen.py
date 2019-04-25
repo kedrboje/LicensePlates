5b99b2fb804eea5f4defb268ab2dbc873dd0b000
kedrboje <fmrubchenkov@edu.hse.ru>
2019-04-25 18:07:38 +0300

    alphabet_gen created; depends on the dataset

---
+++ Alphabet_Gen.py
@@ -0,0 +1,29 @@
+import keras
+import tensorflow as tf
+import os
+import json
+import cv2
+from collections import Counter
+
+
+class Dataset_Generator():
+
+    def __init__(self, tag, dirpath: str = 'dataset/'):
+        self.lplates = list()
+        self.symbols = ''
+        if tag == 'train' or tag == 'test':
+            self.dirpath = os.path.join(dirpath, tag, 'ann')
+            
+    def get_alphabet(self):
+
+        for root, dirs, files in os.walk(self.dirpath):
+            for file in files:
+                ann = json.load(open(os.path.join(root, file), 'r'))
+                tag = ann['tags']
+                desc = ann['description']
+                self.symbols += desc
+        return sorted(Counter(self.symbols).keys())
+
+
+gen = Dataset_Generator(tag='train')
+print(gen.get_alphabet())
