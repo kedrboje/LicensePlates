3f0905a8c9431197d4d81210dd7924880220d607
kedrboje <fmrubchenkov@edu.hse.ru>
2019-04-25 20:37:05 +0300

    added doc, removed debug methods

--- Alphabet_Gen.py
+++ Alphabet_Gen.py
@@ -1,3 +1,10 @@
+"""
+Class Alphabet_Gen consist of two methods:
+    1. get_alphabet() - return sorted array of symbols from the .json files in
+    the dataset/test/ann or dataset/train/ann folders.
+    2. is_valid(string) - return Bool value. True if string consist of
+    characters from the generated alphabet.
+"""
 import keras
 import os
 import json
@@ -11,7 +18,7 @@ class Alphabet_Gen():
         self.symbols = ''
         if tag == 'train' or tag == 'test':
             self.dirpath = os.path.join(dirpath, tag, 'ann')
-          
+
     def get_alphabet(self):
 
         for root, dirs, files in os.walk(self.dirpath):
@@ -22,14 +29,14 @@ class Alphabet_Gen():
                 self.symbols += desc
         return sorted(Counter(self.symbols).keys())
 
-    def is_valid(string):
+    def is_valid(self, string):
 
         for symbol in string:
             if symbol not in self.symbols:
-                raise ValueError
+                # raise ValueError
                 return False
             else:
                 return True
 
-gen = Alphabet_Gen(tag='train')
-print(gen.get_alphabet())
+# gen = Alphabet_Gen(tag='train')
+# print(gen.get_alphabet())
