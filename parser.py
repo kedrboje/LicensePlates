53689688a8c5557bd03cc7ad5e777fa24e99bc34
kedrboje <fmrubchenkov@edu.hse.ru>
2019-05-01 17:21:32 +0300

    added counter for img parsing

--- parser.py
+++ parser.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-
+ 
 
 class img_parser():
-    
+
     def __init__(self, url):
         self.url = url
         try:
@@ -12,19 +12,19 @@ class img_parser():
         except:
             print("Url Error")
 
-def get_links_storage(self, attr: str, links_storage_path: str):
-    self.storage = self.soup.findAll('img')  # here would be your tag
-    with open(links_storage_path, 'a') as f:
-        for link in self.storage[1:]:  # delete [1:] if you need first item
-            f.write(str(link.get(attr)) + "\n")
+    def get_links_storage(self, attr: str, links_storage_path: str):
+        self.storage = self.soup.findAll('img')  # here would be your tag
+        with open(links_storage_path, 'a') as f:
+            for link in self.storage[1:]:  # delete [1:] if you need first item
+                f.write(str(link.get(attr)) + "\n")
 
-def get_img_storage(self, links_storage_path: str, img_storage_path: str, extension=".jpg"):
-    try:
-        with open(links_storage_path, 'r') as f:
-            for index, line in enumerate(f):
-                if line != "None\n":
-                    print(line[:-3])
-                    with open(img_storage_path + str(index) + extension, 'wb') as f_img:
-                        f_img.write(requests.get(line[:-3]).content)
+    def get_img_storage(self, links_storage_path: str, img_storage_path: str, counter: int, extension=".jpg"):
+        try:
+            with open(links_storage_path, 'r') as f:
+                for index, line in enumerate(f):
+                    if line != "None\n":
+                        print(line[:-3])
+                        with open(img_storage_path + str(index + counter) + extension, 'wb') as f_img:
+                            f_img.write(requests.get(line[:-3]).content)
         except:
             pass
