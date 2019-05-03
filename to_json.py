d0f54cb7300c03ba34fa39fda64816fe18d67914
kedrboje <fmrubchenkov@edu.hse.ru>
2019-05-03 20:14:00 +0300

    added helper for JSON

---
+++ to_json.py
@@ -0,0 +1,17 @@
+""" 
+This method changes the .JSON document to make it more readable
+"""
+
+import json
+
+
+def to_json(filename: str, indent=4):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=indent)
+
+path = "model.json"
+
+to_json(path)
