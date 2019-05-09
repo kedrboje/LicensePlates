6ebd877a5bcabeec2cbe05342afaab02bbcd1e19
kedrboje <fmrubchenkov@edu.hse.ru>
2019-05-09 22:42:53 +0300

    last commit before changing inputs

--- Data_Genetator.py
+++ Data_Genetator.py
@@ -15,19 +15,24 @@ class Data_Generator():
         self.img_h = img_h
         if type is "train":
             self.dirpath = os.path.join(dirpath, "train/")
-            self.num = 10822
+            self.num = 10821
         if type is "test":
             self.dirpath = os.path.join(dirpath, "test/")
-            self.num = 562
+            self.num = 561
+        if type is "valid":
+            self.dirpath = os.path.join(dirpath, "valid/")
+            self.num = 21
         self.current = 0
         self.voc = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                     "A", "B", "C", "E", "H", "K", "M", "O", "P", "T", "X", "Y"]
-        self.imgs = np.zeros((self.num, self.img_h, self.img_w))
+        self.imgs = np.empty((self.num, self.img_h, self.img_w))
         self.labels = []
+        self.indexes = list(range(self.num))
 
         for root, dirs, files in os.walk(self.dirpath):
             for i, file in enumerate(files):
                 img = cv2.imread(os.path.join(self.dirpath, file), 0)
+                img = cv2.resize(img, (self.img_w, self.img_h))
                 img = img.astype(np.float32)
                 img /= 255
                 label = os.path.splitext(file)[0]
@@ -38,29 +43,37 @@ class Data_Generator():
         self.current += 1
         if self.current >= self.num:
             self.current = 0
-        return self.imgs[self.current], self.labels[self.current]
+            random.shuffle(self.indexes)
+        return self.imgs[self.indexes[self.current]], self.labels[self.indexes[self.current]]
 
     def encode(self, text):
         return list(map(lambda x: self.voc.index(x), text))
 
     def generate(self):
         while True:
-            x = np.ones([self.batch_size, self.img_h, self.img_w, 1])
-            y = np.ones([self.batch_size, 8])
+            x = np.empty([self.batch_size, self.img_w, self.img_h, 1])
+            y = np.empty([self.batch_size, 8])
             for i in range(self.batch_size):
                 img, label = self._get_sample()
+                img = img.T
                 img = np.expand_dims(img, -1)
                 label = self.encode(label)
                 x[i] = img
                 y[i] = label
             yield (x, y)
 
+# datagen = Data_Generator(type="test", batch_size=32, img_h=34, img_w=152)
+# print(len(datagen.imgs))
+# print(len(datagen.labels))
+# print(len(datagen.indexes))
+# for i, (img, lbl) in enumerate(datagen.generate()):
+#     plt.imshow(img[i, :, :, 0])
+#     plt.xlabel(lbl[i])
+#     plt.show()
+#     if i == 4:
+#         break
+# for i, (a, b) in enumerate(datagen.generate()):
+#     print(a[i], b[i])
+#     i += 1
+#     print(i)
 
-datagen = Data_Generator(type="train", batch_size=32, img_h=34, img_w=152)
-
-for i, (img, lbl) in enumerate(datagen.generate()):
-    plt.imshow(img[i, :, :, 0], cmap='gray')
-    plt.xlabel(lbl[i])
-    plt.show()
-    if i == 4:
-        break
