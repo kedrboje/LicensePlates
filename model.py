5561c4857ff5444907073c8073fd4b3215b9799d
kedrboje <fmrubchenkov@edu.hse.ru>
2019-05-10 23:26:03 +0300

    last trained model with bug fixes

--- model.py
+++ model.py
@@ -23,19 +23,20 @@ train_datagen = Data_Generator(type="train",
                                batch_size=batch_size,
                                img_h=img_h,
                                img_w=img_w,
-                               downsample=4)
+                               downsample=4,
+                               max_lbl_len=9)
 
-test_datagen = Data_Generator(type="test",
-                              batch_size=batch_size,
-                              img_h=img_h,
-                              img_w=img_w,
-                              downsample=4)
+valid_datagen = Data_Generator(type="valid",
+                               batch_size=batch_size,
+                               img_h=img_h,
+                               img_w=img_w,
+                               downsample=4,
+                               max_lbl_len=9)
 
 conv_to_rnn = (img_w // 4, (img_h // 4) * 16)
 
 input_sample = layers.Input(name='input', shape=(img_w, img_h, 1))
 
-# Convolution base
 conv = layers.Conv2D(16, (3, 3), padding="same", activation="relu", kernel_initializer='he_normal')(input_sample)
 
 max_pool_first = layers.MaxPooling2D((2, 2))(conv)
@@ -60,9 +61,9 @@ gru_2_backwards = layers.GRU(512, return_sequences=True, go_backwards=True, kern
 
 dense_2 = layers.Dense(23, kernel_initializer='he_normal')(keras.layers.merge.concatenate([gru_2, gru_2_backwards]))
 
-act = layers.Activation('softmax')(dense_2)
+act = layers.Activation('softmax', name='softmax')(dense_2)
 
-labels = layers.Input(name='labels', shape=[8], dtype='float32')
+labels = layers.Input(name='labels', shape=[train_datagen.max_lbl_len], dtype='float32')
 input_len = layers.Input(name='input_len', shape=[1], dtype='int64')
 label_len = layers.Input(name='label_len', shape=[1], dtype='int64')
 
@@ -73,6 +74,8 @@ def ctc_lambda_func(args):
 
 loss_out = layers.Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([act, labels, input_len, label_len])
 
+sgd = keras.optimizers.SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
+
 ###
 
 model = models.Model(inputs=[input_sample, labels, input_len, label_len], outputs=loss_out)
@@ -80,16 +83,17 @@ model = models.Model(inputs=[input_sample, labels, input_len, label_len], output
 model.summary()
 
 
-model.compile(optimizer='rmsprop',
+model.compile(optimizer=sgd,
               loss={'ctc': lambda y_true, act: act},
               metrics=['acc'])
 
+test_func = K.function([input_sample], [act])
 
 history = model.fit_generator(train_datagen.generate(),
-                              steps_per_epoch=10821,
+                              steps_per_epoch=4392,
                               epochs=1,
-                              validation_data=test_datagen.generate(),
-                              validation_steps=561)
+                              validation_data=valid_datagen.generate(),
+                              validation_steps=546)
 
 model.save('sex_model.h5')
 
@@ -103,14 +107,3 @@ model.save_weights("sex_model_weights.h5")
 print("Saved model to disk")
 
 
-acc = history.history['acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs = range(1, len(acc) + 1)
-plt.legend()
-plt.figure()
-plt.plot(epochs, loss, 'bo', label='Training loss') 
-plt.plot(epochs, val_loss, 'b', label='Validation loss') 
-plt.title('Training and validation loss')
-plt.legend()
-plt.show()
